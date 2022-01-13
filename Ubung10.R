cc6177092c2b825fa07c77b62d4edbc0d0199bbc
Ye Yuan <yuany@cip.ifi.lmu.de>
2022-01-13 15:18:41 +0100

    add modifications

--- Ubung10.R
+++ Ubung10.R
@@ -18,4 +18,26 @@ pre <-predict(m)
 table <-as.data.table(res,pre)
 ggplot(table, aes(x=pre,y=res)) + geom_point() +geom_hline(yintercept = 0)
 ggplot(table,aes(sample = res)) + geom_qq() + geom_qq_line() #to see the distribution(gaussian distribution), we can then be confident that we are using linear regression because it requires normal distribution
+mheight <- heights[sex %in% c("M")]
+fheight <- heights[sex %in% c("F")]
+mlm <- lm(height ~ father, data = mheight)
+flm <- lm(height ~ father, data = fheight)
+ggplot(mheight, aes(x= father, height)) + geom_point() + geom_smooth(method = "lm")
+ggplot(fheight, aes(x = father,height)) + geom_point()  + geom_smooth(method = "lm")+ geom_line(aes(father,height,color = "predicted father"))
+
+pca_pbj <- princomp(mheight[,.(height,father)])
+pca_pbj$loadings
+slope <- pca_pbj$loadings["height","Comp.1"] / pca_pbj$loadings["father","Comp.1"]
+intercept <- pca_pbj$center["height"] -slope* pca_pbj$center["father"]
+geom_abline(aes(intercept = intercept, slope = slope ,color = "pc_1"))
+#PCA basically minize along the axis y and x.
+#in the PCA, Sum of squares along all the axises are minimized.
+
+#section 2
+growth <- genotype_growth %>% melt(id.vars="strain", variable.name='media', value.name='growth_rate')
+growth <- growth[media=="YPMalt"]
+genotype <- genotype[, c(strain, "mrk_5211", "mrk_5091")]
+full <- lm(growth_rate ~ mrk_5211 + mrk_5091)
+
+
 
