7f35e7766d4f5c66e64df68245770d9c19fd5716
Ye Yuan <yuany@cip.ifi.lmu.de>
2022-01-27 15:26:21 +0100

    add modification

--- Ubung12.R
+++ Ubung12.R
@@ -30,6 +30,27 @@ diabetes_dt[,predict_Dt := predicteddata]#minimum observations in one leaf,
 ggroc <- ggplot(diabetes_dt, aes(d = as.numeric(Outcome), m = predict_Dt)) + geom_roc() + geom_abline()
 ggroc
 set.seed(13)
-#Observations
+#Observations, floor function rounding down the decimal number
 smp_size <- floor(0.7 * nrow(diabetes_dt))
 train_ind <- sample(seq_len(nrow(diabetes_dt)), size = smp_size)
+#build dt_classifier
+dt_classifer <- rpart(full_formula,data = diabetes_dt[train_ind],control = rpart.control(minsplit = 3, cp = 0.001))
+#mark test & training data
+diabetes_dt[, predictedd := predict(dt_classifer,type ="prob", 
+                                              newdata = diabetes_dt)[,2]]
+diabetes_dt[train_ind, dataset := "train"]
+diabetes_dt[-train_ind, dataset := "test"]
+ggroc <- ggplot(diabetes_dt, aes(d=as.numeric(Outcome), m = predictedd, color = dataset)) + geom_roc() + geom_abline()
+ggroc
+rf_classifier <- randomForest(formula= full_formula,data = diabetes_dt[train_ind],
+                              ntree = 200,#number of trees
+                              nodesize =20, #minimum size of a leaf node, 
+                              maxnodes = 7,#maximum number of leaf node
+                              mtry =5) #number of featured variables considered for the trees)
+rf_classifier
+diabetes_dt[,rfpredict := predict(rf_classifier, type = "prob", newdata = diabetes_dt)[,2]]
+ggrf <- ggplot(diabetes_dt, aes(d= as.numeric(Outcome), m = rfpredict, color = dataset)) + geom_roc() + geom_abline()
+ggrf
+
+#Section 5
+
